c9ce4ba441e6d54ca4d0768b6c573dd483a18af5
Stas Zhabunin <63603336+xdhao@users.noreply.github.com>
2024-10-22 12:03:45 +0300

    Update schedule.py

--- app/views/schedule.py
+++ app/views/schedule.py
@@ -145,18 +145,17 @@ def access1():
     a = datetime.now()
     d = timedelta(hours=3)
     c = a + d
-    global test3
-    connection = pymysql.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWORD, database=DATABASE_NAME)
-    cursor = connection.cursor()
-
-
-    i = 0
     timez = c.strftime("%H:%M")
-    cursor.execute("SELECT * from stud_access WHERE time_end>(%s) order by time_begin", timez)
-    tmp = cursor.fetchall()
 
-    print(len(tmp))
-    #test4 = data.Stud_access.query.filter(data.Stud_access.time_begin > timez).order_by(data.Stud_access.time_begin).all()
+    connection = pymysql.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWORD,
+                                 database=DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM stud_access WHERE time_end > %s ORDER BY time_begin", (timez,))
+    tmp = cursor.fetchall()
+    connection.close()
+
+    # Вывод количества записей
+    print(f"Количество записей: {len(tmp)}")
 
 
 def access():
@@ -165,154 +164,122 @@ def access():
     c = a + d
     d1 = c.strftime("%d.%m.%Y")
     global test3
+
+    # Открытие сессии для SOAP клиента
     client = Client(wsdl=f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl")
-    domain = "SYSTEM"
-    nameuser = "parsec"
-    password = "parsec"
-    session = (client.service.OpenSession(domain, nameuser, password))
+    session = client.service.OpenSession(domain, nameuser, password)
     sessionID = session.Value.SessionID
-    lastname = "Student"
-    pers1 = (client.service.FindPeople(sessionID, lastname))
+
     timez = c.strftime("%H:%M")
-    connection = pymysql.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWORD, database=DATABASE_NAME)
+
+    # Подключение к базе данных и выборка пользователей
+    connection = pymysql.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWORD,
+                                 database=DATABASE_NAME)
     cursor = connection.cursor()
-    cursor.execute("SELECT * from stud_access WHERE time_end>(%s) AND time_begin<=(%s) AND date=(%s) order by time_begin", (timez, timez, d1) )
+    cursor.execute(
+        "SELECT * from stud_access WHERE time_end > %s AND time_begin <= %s AND date = %s ORDER BY time_begin",
+        (timez, timez, d1))
     test4 = cursor.fetchall()
     connection.close()
 
-    #test4 = data.Stud_access.query.filter(data.Stud_access.time_begin > timez).order_by(data.Stud_access.time_begin).all()
+    # Если произошли изменения в пользователях
     if test3 != test4:
-
-        test5 = get_unique_numbers(test3, test4) #добавляемые пользователи
-
-        test6 = get_numbers(test3, test4) #удаляемые пользователи
-
+        test5 = get_unique_numbers(test3, test4)  # Новые пользователи
+        test6 = get_numbers(test3, test4)  # Удаляемые пользователи
         test3 = test4
-        g = 0
-        while g < len(test5):
-            client = Client(wsdl=f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl")
-            domain = "SYSTEM"
-            nameuser = "parsec"
-            password = "parsec"
-            session = (client.service.OpenSession(domain, nameuser, password))
-            sessionID = session.Value.SessionID
-            lastname = "Student"
-            firstname = test5[g][1]
 
-            buf = (client.service.FindPeople(sessionID, lastname, firstname))
+        # Обработка добавляемых пользователей
+        for student in test5:
+            firstname = student[1]  # Используем вторую колонку как имя
+            buf = client.service.FindPeople(sessionID, "Student", firstname)
+            if buf and len(buf) > 0:
+                PERSON_ID = buf[0].ID
+                code1 = client.service.GetPersonIdentifiers(sessionID, PERSON_ID)
+                CODE = code1[0].CODE
+                personEditSessionID = client.service.OpenPersonEditingSession(sessionID, PERSON_ID).Value
 
-            PERSON_ID = buf[0].ID
-
-            code1 = (client.service.GetPersonIdentifiers(sessionID, PERSON_ID))
-            CODE = code1[0].CODE
-            session = (client.service.OpenPersonEditingSession(sessionID, PERSON_ID))
-            personEditSessionID = (session.Value)
-
-            endpoint = f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl"
-            IS_PRIMARY = 1
-            NAME = "Этажи"  # name территории
-
-            ter = (client.service.GetAccessGroups(sessionID))  # список всех территорий
-            matches = [el for el in ter if el.NAME == NAME]  # поиск территории по имени
-            ACCGROUP_ID = matches[0].ID
-
-            login_template = """
-                            <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-                            <soap:Body>
-                            <AddPersonIdentifier xmlns="http://parsec.ru/Parsec3IntergationService">
-                            <personEditSessionID>{personEditSessionID}</personEditSessionID>
-                            <identifier xsi:type="Identifier">
-                            <CODE>{CODE}</CODE>
-                            <PERSON_ID>{PERSON_ID}</PERSON_ID>
-                            <IS_PRIMARY>{IS_PRIMARY}</IS_PRIMARY>
-                            <ACCGROUP_ID>{ACCGROUP_ID}</ACCGROUP_ID>
-                            </identifier>
-                            </AddPersonIdentifier>
-                            </soap:Body>
-                            </soap:Envelope>
-                            """
-
-            body = login_template.format(personEditSessionID=personEditSessionID, CODE=CODE, PERSON_ID=PERSON_ID,
-                                         IS_PRIMARY=IS_PRIMARY, ACCGROUP_ID=ACCGROUP_ID)
-            body = body.encode('utf-8')
-
-            session = requests.session()
-            session.headers = {"Content-Type": "text/xml; charset=utf8"}
-            session.headers.update({"Content-Length": str(len(body))})
-            response = session.post(url=endpoint, data=body, verify=False)
-            #print(response.content.decode("utf-8"))
-            #print(response.status_code)
-            #print(test5)
-            print(timez)
-            g += 1
-
-
-
-
-
-
-        i = 0
-        while i < len(test6):
-
-                    client = Client(wsdl=f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl")
-
-                    domain = "SYSTEM"
-                    nameuser = "parsec"
-                    password = "parsec"
-                    session = (client.service.OpenSession(domain, nameuser, password))
-                    sessionID = session.Value.SessionID
-                    lastname = "Student"
-                    firstname = test6[i][1]
-
-                    buf = (client.service.FindPeople(sessionID, lastname, firstname))
-                    PERSON_ID = buf[0].ID
-                    code1 = (client.service.GetPersonIdentifiers(sessionID, PERSON_ID))
-
-                    CODE = code1[0].CODE
-
-                    session = (client.service.OpenPersonEditingSession(sessionID, PERSON_ID))
-                    personEditSessionID = (session.Value)
-
-                    endpoint = f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl"
-                    IS_PRIMARY = 1
-                    NAME = test6[i][2]  # name территории
-
-                    ter = (client.service.GetAccessGroups(sessionID))  # список всех территорий
-                    matches = [el for el in ter if el.NAME == NAME]  # поиск территории по имени
+                # Получение ID территории по имени
+                NAME = "Этажи"
+                ter = client.service.GetAccessGroups(sessionID)
+                matches = [el for el in ter if el.NAME == NAME]
+                if matches:
                     ACCGROUP_ID = matches[0].ID
 
-                    login_template = """
-                    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-                    <soap:Body>
-                    <AddPersonIdentifier xmlns="http://parsec.ru/Parsec3IntergationService">
-                    <personEditSessionID>{personEditSessionID}</personEditSessionID>
-                    <identifier xsi:type="Identifier">
-                    <CODE>{CODE}</CODE>
-                    <PERSON_ID>{PERSON_ID}</PERSON_ID>
-                    <IS_PRIMARY>{IS_PRIMARY}</IS_PRIMARY>
-                    <ACCGROUP_ID>{ACCGROUP_ID}</ACCGROUP_ID>
-                    </identifier>
-                    </AddPersonIdentifier>
-                    </soap:Body>
-                    </soap:Envelope>
-                    """
+                # Формируем и отправляем SOAP запрос для добавления идентификатора
+                login_template = """
+                <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+                <soap:Body>
+                <AddPersonIdentifier xmlns="http://parsec.ru/Parsec3IntergationService">
+                <personEditSessionID>{personEditSessionID}</personEditSessionID>
+                <identifier xsi:type="Identifier">
+                <CODE>{CODE}</CODE>
+                <PERSON_ID>{PERSON_ID}</PERSON_ID>
+                <IS_PRIMARY>1</IS_PRIMARY>
+                <ACCGROUP_ID>{ACCGROUP_ID}</ACCGROUP_ID>
+                </identifier>
+                </AddPersonIdentifier>
+                </soap:Body>
+                </soap:Envelope>
+                """
+                body = login_template.format(personEditSessionID=personEditSessionID, CODE=CODE, PERSON_ID=PERSON_ID,
+                                             ACCGROUP_ID=ACCGROUP_ID)
+                body = body.encode('utf-8')
 
-                    body = login_template.format(personEditSessionID=personEditSessionID, CODE=CODE, PERSON_ID=PERSON_ID,
-                                                 IS_PRIMARY=IS_PRIMARY, ACCGROUP_ID=ACCGROUP_ID)
-                    body = body.encode('utf-8')
+                session = requests.session()
+                session.headers = {"Content-Type": "text/xml; charset=utf8"}
+                session.headers.update({"Content-Length": str(len(body))})
+                response = session.post(url=f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl",
+                                        data=body, verify=False)
 
-                    session = requests.session()
-                    session.headers = {"Content-Type": "text/xml; charset=utf8"}
-                    session.headers.update({"Content-Length": str(len(body))})
-                    response = session.post(url=endpoint, data=body, verify=False)
-                    #print(response.content.decode("utf-8"))
-                    #print(response.status_code)
-                    #print(CODE + "B")
-                    print(timez)
-                    i+=1
+                print(f"Добавлен идентификатор для {firstname} на {NAME} в {timez}")
 
+        # Обработка удаляемых пользователей
+        for student in test6:
+            firstname = student[1]
+            NAME = student[2]
+
+            buf = client.service.FindPeople(sessionID, "Student", firstname)
+            if buf and len(buf) > 0:
+                PERSON_ID = buf[0].ID
+                code1 = client.service.GetPersonIdentifiers(sessionID, PERSON_ID)
+                CODE = code1[0].CODE
+                personEditSessionID = client.service.OpenPersonEditingSession(sessionID, PERSON_ID).Value
+
+                ter = client.service.GetAccessGroups(sessionID)
+                matches = [el for el in ter if el.NAME == NAME]
+                if matches:
+                    ACCGROUP_ID = matches[0].ID
+
+                # Формируем и отправляем SOAP запрос для удаления идентификатора
+                login_template = """
+                <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+                <soap:Body>
+                <AddPersonIdentifier xmlns="http://parsec.ru/Parsec3IntergationService">
+                <personEditSessionID>{personEditSessionID}</personEditSessionID>
+                <identifier xsi:type="Identifier">
+                <CODE>{CODE}</CODE>
+                <PERSON_ID>{PERSON_ID}</PERSON_ID>
+                <IS_PRIMARY>1</IS_PRIMARY>
+                <ACCGROUP_ID>{ACCGROUP_ID}</ACCGROUP_ID>
+                </identifier>
+                </AddPersonIdentifier>
+                </soap:Body>
+                </soap:Envelope>
+                """
+                body = login_template.format(personEditSessionID=personEditSessionID, CODE=CODE, PERSON_ID=PERSON_ID,
+                                             ACCGROUP_ID=ACCGROUP_ID)
+                body = body.encode('utf-8')
+
+                session = requests.session()
+                session.headers = {"Content-Type": "text/xml; charset=utf8"}
+                session.headers.update({"Content-Length": str(len(body))})
+                response = session.post(url=f"http://{SOAP_HOST}/IntegrationService/IntegrationService.asmx?wsdl",
+                                        data=body, verify=False)
+
+                print(f"Удален идентификатор для {firstname} с {NAME} в {timez}")
 
 
 def schedule1():
-
     schedule.every(5).seconds.do(access)
+
+
